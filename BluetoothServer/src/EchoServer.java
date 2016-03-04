a6ad49a939a802d2225d33622bf7de112bd2edc4
Gabriel T <gabriel.tapuc@gmail.com>
2016-03-03 23:33:16 -0500

    Now able to successfully receive a message from Android!

--- BluetoothServer/src/EchoServer.java
+++ BluetoothServer/src/EchoServer.java
@@ -8,9 +8,10 @@ import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
 import java.io.DataInputStream;
+import java.io.IOException;
 
 public class EchoServer {
-    public final UUID uuid = new UUID("27012f0c68af4fbf8dbe6bbaf7aa432a", false);
+    public final UUID uuid = new UUID("40850b4ade8811e5b86d9a79f06e9478", false);
     public final String name = "Echo Server";
     public final String url = "btspp://localhost:"
                                 + uuid
@@ -36,13 +37,19 @@ public class EchoServer {
 
             System.out.println("Client Connected...");
             DataInputStream din = new DataInputStream(conn.openInputStream());
+
+            final int BUFFER_SIZE = 1024;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytes = 0;
+            int b = BUFFER_SIZE;
             while(true){
-                String cmd = "";
-                char c;
-                while (((c = din.readChar()) > 0) && (c != '\n')){
-                    cmd = cmd + c;
+                try {
+                    bytes = din.read(buffer, bytes, BUFFER_SIZE - bytes);
+                    String asdf = new String(buffer);
+                    System.out.println(asdf);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                System.out.println("Received " + cmd);
             }
         }
         catch (Exception e) {System.out.println("Exception Occured: " + e.toString());}
